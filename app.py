d34e16c017048edca4656e31985ed297c0bbf2d8
Sara Bsharat <115897580+sarabsharat@users.noreply.github.com>
2025-01-20 01:04:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,35 @@ def index():
 def webcam():
     return render_template('Webcam.html')
 
+#صثلاؤشة رهي
+def generate_frames():
+    camera = cv2.VideoCapture(0)  # Open the webcam
+    while True:
+        success, frame = camera.read()  # Read a frame from the webcam
+        if not success:
+            break
+        
+        # Perform object detection
+        results = model(frame)
+        annotated_frame = results[0].plot()  # Annotate the frame with detection results
+        
+        # Encode the frame in JPEG format
+        ret, buffer = cv2.imencode('.jpg', annotated_frame)
+        frame = buffer.tobytes()
+        
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # Yield the frame
+
+@app.route('/')
+def index():
+    return render_template('index.html')  # Render the main page
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+#gggggggggggggggggggggggggggggggggggggggggggg
+
 @app.route('/upload', methods=['POST'])
 def upload_file():
     if 'file' not in request.files:
@@ -79,3 +108,6 @@ def send_detected_file(filename):
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+#hsgdsa  if __name__ == '__main__':   app.run(host='0.0.0.0', port=5000)
+
